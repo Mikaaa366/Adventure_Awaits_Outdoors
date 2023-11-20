c658ec15d7b8f4dd4c4a6eadcd01d488cde3b405
Dominika Janisz <mikaaa366@gmail.com>
2023-11-21 00:49:25 +0100

    rebuild

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,14 +17,14 @@ import configuration from './config/configuration';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../', 'client', 'build'),
-      serveRoot: '/static',
+      rootPath: join(__dirname, '../..', 'client', 'build'),
+      //serveRoot: '/static',
     }),
     ProductsModule,
     OrdersModule,
     PrismaModule,
-    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
   ],
   controllers: [AppController],
   providers: [AppService],
